c99950323af706c43073ab8d8456271231bc7578
Dylan Warrener <dqyqlqaqn@gmail.com>
2023-05-05 14:28:13 +0100

    REmoved comments

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -3,30 +3,23 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
-	// The entry file to start compilation
-	entry: "./src/index.ts",
+	context: __dirname,
+	entry: {
+		bundle: path.resolve(__dirname, "./src/index.ts"),
+	},
 	output: {
-		// Names the output file to bundle.js
 		filename: "bundle.js",
-		// Directs all compilation files to the dist folder
 		path: path.resolve(__dirname, "dist"),
-		// Not sure yet
 		publicPath: "dist",
-		// Cleans the dist folder on compilation
 		clean: true,
 	},
 	devServer: {
-		// Tells dev server where to serve files from
 		static: {
 			directory: path.join(__dirname, "dist"),
 		},
-		// Only show webpack errors, instead of a long console log message.
 		stats: "errors-only",
-		// Open a new browser when webpack dev server loads
 		open: true,
-		// GZIPS the files before serving them
 		compress: true,
-		// Shows a fullscreen overlay in the browser when there are compiler errors but not warnings
 		client: {
 			overlay: {
 				errors: true,
@@ -54,6 +47,9 @@ module.exports = {
 			{
 				test: /\.js$/,
 				loader: "babel-loader",
+				options: {
+					appendTsSuffixTo: [/\.vue$/],
+				},
 				exclude: /node_modules/,
 			},
 			// Applies to both plain `.ts` files
@@ -61,7 +57,6 @@ module.exports = {
 			{
 				test: /\.ts$/,
 				loader: "ts-loader",
-				options: { appendTsSuffixTo: [/\.vue$/] },
 				exclude: /node_modules/,
 			},
 			// Applies to both plain `.css` or `.scss` files
@@ -73,6 +68,7 @@ module.exports = {
 		],
 	},
 	resolve: {
+		modules: ["node_modules"],
 		extensions: [".js", ".ts", ".vue"],
 	},
 	plugins: [

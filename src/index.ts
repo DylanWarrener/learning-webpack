3c1ad2f5b6f0c548ea97963a5e707a2bcdd07281
DylanWarrener <dqyqlqaqn@gmail.com>
2023-05-07 16:38:53 +0100

    Changed reference to the correct app.vue file

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
-import App from "./app.vue";
+import App from "./App.vue";
 
 const app = createApp(App);
 const pinia = createPinia();

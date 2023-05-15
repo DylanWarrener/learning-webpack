d87fde22caf165ce2922e4c8a1c7b60de4c96f89
DylanWarrener <dqyqlqaqn@gmail.com>
2023-05-15 08:06:34 +0100

    Removed header & footer to see routes

---
+++ src/main.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="container">
+		<!--<header-component></header-component>-->
+		<router-view></router-view>
+		<!--<footer-component></footer-component>-->
+	</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+//import Header from './components/header/header.vue';
+//import Footer from './components/footer/footer.vue';
+
+export default defineComponent({
+	name: "app",
+	components: {
+		//'header-component': Header,
+		//'footer-component': Footer
+	}
+});
+</script>
+
+<style scoped lang="scss">
+.container {
+	border: 2px solid red;
+}
+</style>

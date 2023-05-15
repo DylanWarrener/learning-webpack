2bb4fa690334e6ff4926603cf20f136fa0001d42
DylanWarrener <dqyqlqaqn@gmail.com>
2023-05-15 08:05:43 +0100

    Delete prefech plugin

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,16 @@
 const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
-	transpileDependencies: true,
-	css: {
-		loaderOptions: {
-			sass: {
-				data: `@import "./styles.scss";`,
-			},
-		},
-	},
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        data: `@import "./styles.scss";`,
+      },
+    },
+  },
+  // Delete the prefetch plugin
+  chainWebpack: (config) => {
+    config.plugins.delete("prefetch");
+  },
 });
